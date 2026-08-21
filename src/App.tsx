a6a2d194de6d173ca337d6089fba71f6055054d6
Radoslav Todorov <todorovradoslav18@gmail.com>
2026-08-21 05:15:12 +0300

    init components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import Header from './components/header/Header'
+import Hero from './components/hero/Hero'
 import NavMarketing from './components/marketing/nav-marketing/NavMarketing'
+import StrongSides from './components/marketing/strong-sides/StrongSides'
+import WorkWithUs from './components/marketing/work-with-us/WorkWithUs'
+import Stats from './components/stats/Stats'
 
 
 function App() {
@@ -9,6 +13,10 @@ function App() {
     <>
     <NavMarketing />
     <Header />
+    <Hero />
+    <StrongSides />
+    <WorkWithUs />
+    <Stats />
     </>
   )
 }
